a81a80c4c16793d8ec9d2170c33038193b536ae0
ViperMSK <vgpershin@yandex.ru>
2022-07-14 15:55:54 +0300

    Image Loading by Coil

--- app/src/main/java/com/example/kase/MainActivity.kt
+++ app/src/main/java/com/example/kase/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.example.kase.databinding.ActivityMainBinding
 import com.example.kase.network.MarsApi
+import com.example.kase.network.MarsPhoto
 import kotlinx.coroutines.launch
 
 /**
@@ -13,6 +16,7 @@ import kotlinx.coroutines.launch
  * viewBinding & dataBinding (lifecycleScope)
  * implementation 'com.squareup.retrofit2:converter-moshi:2.9.0'
  * implementation 'com.squareup.moshi:moshi-kotlin:1.13.0'
+ * implementation "io.coil-kt:coil:1.1.1"
  */
 
 class MainActivity : AppCompatActivity() {
@@ -27,11 +31,18 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             try {
                 binding.progressBar.isVisible = true
-                binding.textView.text = MarsApi.retrofitService.getPhotos().toString()
+                val allPhotos: List<MarsPhoto> = MarsApi.retrofitService.getPhotos()
                 binding.progressBar.isVisible = false
+
+                val firstImgUrl = allPhotos[0].imageSrcUrl
+                val imgUri = firstImgUrl.toUri().buildUpon().scheme("https").build()
+
+                binding.imageView.load(imgUri) {
+                    placeholder(R.drawable.loading_animation)
+                    error(R.drawable.ic_broken_image)
+                }
             } catch (e: Exception) {
-                binding.progressBar.isVisible = false
-                binding.textView.text = "Unable to load data!"
+                binding.imageView.setImageResource(R.drawable.ic_broken_image)
             }
         }
     }
