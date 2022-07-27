b9c4e4364d9cd9899be3f2c979420d4ab6d23e0d
ViperMSK <vgpershin@yandex.ru>
2022-07-27 15:13:03 +0300

    Activities diff mode navigation

--- app/src/main/java/com/example/kase/GreenActivity.kt
+++ app/src/main/java/com/example/kase/GreenActivity.kt
@@ -3,51 +3,43 @@ package com.example.kase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kase.databinding.ActivityGreenBinding
 import com.example.kase.databinding.ActivityMainBinding
 
-/**
- * android:launchMode=""
- * android:label=""
- * android:parentActivityName=".MainActivity" (back press navigation)
- */
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class GreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityGreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val message = intent.extras?.get("KEY")
-        if (message != null) {
-            binding.textView.text = "${binding.textView.text} + $message"
-        }
+        binding.textView.text = "${binding.textView.text} + $message"
 
         binding.buttonMain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from GreenActivity!")
             startActivity(intent)
         }
 
         binding.buttonRed.setOnClickListener {
             val intent = Intent(this, RedActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from GreenActivity!")
             startActivity(intent)
         }
         binding.buttonGreen.setOnClickListener {
             val intent = Intent(this, GreenActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from GreenActivity!")
             startActivity(intent)
         }
         binding.buttonBlue.setOnClickListener {
             val intent = Intent(this, BlueActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from GreenActivity!")
             startActivity(intent)
         }
         binding.buttonYellow.setOnClickListener {
             val intent = Intent(this, YellowActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from GreenActivity!")
             startActivity(intent)
         }
         binding.buttonExit.setOnClickListener {
--- app/src/main/java/com/example/kase/YellowActivity.kt
+++ app/src/main/java/com/example/kase/YellowActivity.kt
@@ -4,54 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kase.databinding.ActivityMainBinding
+import com.example.kase.databinding.ActivityYellowBinding
 
-/**
- * android:launchMode=""
- * android:label=""
- * android:parentActivityName=".MainActivity" (back press navigation)
- */
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class YellowActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityYellowBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityYellowBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val message = intent.extras?.get("KEY")
-        if (message != null) {
-            binding.textView.text = "${binding.textView.text} + $message"
-        }
+        binding.textView.text = "${binding.textView.text} + $message"
 
         binding.buttonMain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from YellowActivity!")
             startActivity(intent)
         }
 
         binding.buttonRed.setOnClickListener {
             val intent = Intent(this, RedActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from YellowActivity!")
             startActivity(intent)
         }
         binding.buttonGreen.setOnClickListener {
             val intent = Intent(this, GreenActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from YellowActivity!")
             startActivity(intent)
         }
         binding.buttonBlue.setOnClickListener {
             val intent = Intent(this, BlueActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from YellowActivity!")
             startActivity(intent)
         }
         binding.buttonYellow.setOnClickListener {
             val intent = Intent(this, YellowActivity::class.java)
-            intent.putExtra("KEY", "Hello from MainActivity!")
+            intent.putExtra("KEY", "Hello from YellowActivity!")
             startActivity(intent)
         }
         binding.buttonExit.setOnClickListener {
             finish()
         }
+
     }
 }
